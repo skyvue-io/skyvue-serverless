2a84c4d18467e76595072c5119fa4557a64f6299
Tristan Tarpley <tristan@learnwithjuni.com>
2021-03-15 21:13:14 -0500

    asf

--- upload_dataset/index.js
+++ upload_dataset/index.js
@@ -1,14 +1,9 @@
 const randomWords = require('random-words');
 
 exports.handler = async event => {
-  try {
-    const { body } = JSON.parse(event);
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify(body),
-    };
-    return response;
-  } catch (e) {
-    return e;
-  }
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(event.body),
+  };
+  return response;
 };
