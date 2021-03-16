3c92786695aa91bd677025e928e33987934300df
Tristan Tarpley <tristan@learnwithjuni.com>
2021-03-15 21:43:44 -0500

    trying something else

--- upload_dataset/index.js
+++ upload_dataset/index.js
@@ -1,44 +1,58 @@
 const mongoose = require('mongoose');
-const Dataset = require('./models/Dataset');
+// const Dataset = require('./models/Dataset');
 
 require('dotenv').config();
 
-const handleMongoError = err => {
-  console.error(`Mongoose connection error: ${err}`);
-  process.exit(1);
-};
+// const handleMongoError = err => {
+//   console.error(`Mongoose connection error: ${err}`);
+//   process.exit(1);
+// };
 
-const connectToDB = async () => {
-  await mongoose
-    .connect(process.env.DB_URI, {
-      useNewUrlParser: true,
-      useFindAndModify: false,
-      useCreateIndex: true,
-      useUnifiedTopology: true,
-    })
-    .catch(handleMongoError);
+// const connectToDB = async () => {
+//   await mongoose
+//     .connect(process.env.DB_URI, {
+//       useNewUrlParser: true,
+//       useFindAndModify: false,
+//       useCreateIndex: true,
+//       useUnifiedTopology: true,
+//     })
+//     .catch(handleMongoError);
 
-  mongoose.connection.on('error', handleMongoError);
-}
+//   mongoose.connection.on('error', handleMongoError);
+// }
 
-connectToDB();
+// connectToDB();
+let conn = null;
 
-exports.handler = async event => {
-  const dataset = new Dataset({
-    userId: 'blah',
-    title: 'blah',
-    visibilitySettings: {
-      owner: 'blah',
-      editors: ['blah'],
-      viewers: ['blah'],
-    },
-  })
+exports.handler = async (event, context) => {
+  context.callbackWaitsForEmptyEventLoop = false;
 
-  dataset.save();
+  // Because `conn` is in the global scope, Lambda may retain it between
+  // function calls thanks to `callbackWaitsForEmptyEventLoop`.
+  // This means your Lambda function doesn't have to go through the
+  // potentially expensive process of connecting to MongoDB every time.
+  if (conn == null) {
+    conn = mongoose.createConnection(process.env.DB_URI, {
+      // Buffering means mongoose will queue up operations if it gets
+      // disconnected from MongoDB and send them when it reconnects.
+      // With serverless, better to fail fast if not connected.
+      bufferCommands: false, // Disable mongoose buffering
+      bufferMaxEntries: 0 // and MongoDB driver buffering
+    });
+
+    // `await`ing connection after assigning to the `conn` variable
+    // to avoid multiple function calls creating new connections
+    await conn;
+    conn.model('Test', new mongoose.Schema({ name: String }));
+  }
+
+  const M = conn.model('Test');
+
+  new M({ name: 'testing' }).save();
 
   const response = {
     statusCode: 200,
-    body: JSON.stringify(dataset._id),
+    body: JSON.stringify('let\'s try this'),
   };
   return response;
 };
