621e2954aeb1c7e11b43f1cdab140e5e75bb930c
Tristan Tarpley <tristan@learnwithjuni.com>
2021-03-15 21:23:45 -0500

    testing env variables

--- upload_dataset/index.js
+++ upload_dataset/index.js
@@ -1,9 +1,32 @@
-const randomWords = require('random-words');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const handleMongoError = err => {
+  console.error(`Mongoose connection error: ${err}`);
+  process.exit(1);
+};
+
+const connectToDB = async () => {
+  await mongoose
+    .connect(process.env.DB_URI, {
+      useNewUrlParser: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+    })
+    .catch(handleMongoError);
+
+  mongoose.connection.on('error', handleMongoError);
+}
+
+connectToDB();
 
 exports.handler = async event => {
+  const body = JSON.parse(event.body);
   const response = {
     statusCode: 200,
-    body: JSON.stringify(JSON.parse(event.body)),
+    body: JSON.stringify(process.env.TESTING),
   };
   return response;
 };
