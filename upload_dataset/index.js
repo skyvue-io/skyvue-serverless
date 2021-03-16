75fe53feebab505725eaa1e8bf90e501b02b4723
Tristan Tarpley <tristan@learnwithjuni.com>
2021-03-15 21:13:53 -0500

    blah

--- upload_dataset/index.js
+++ upload_dataset/index.js
@@ -3,7 +3,7 @@ const randomWords = require('random-words');
 exports.handler = async event => {
   const response = {
     statusCode: 200,
-    body: JSON.stringify(event.body),
+    body: JSON.stringify(JSON.parse(event.body)),
   };
   return response;
 };
