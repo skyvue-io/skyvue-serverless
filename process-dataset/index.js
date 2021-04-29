2e67659d93ec56202bba51b6d86271e2b832f79f
Tristan Tarpley <tristan@learnwithjuni.com>
2021-04-28 21:04:03 -0500

    set up for process-dataset

---
+++ process-dataset/index.js
@@ -0,0 +1,13 @@
+exports.handler = async event => {
+  const bucket = event.Records[0].s3.bucket.name;
+  const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
+
+  console.log(bucket, key);
+  // TODO implement
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(event),
+  };
+
+  return response;
+};
