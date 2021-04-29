c711639887292993e4598982e910eec068f28807
Tristan Tarpley <tristan@learnwithjuni.com>
2021-04-28 22:46:05 -0500

    remove excess

--- process-dataset/index.js
+++ process-dataset/index.js
@@ -24,17 +24,8 @@ const makeRedshift = async () => {
   return client;
 };
 
-exports.handler = async event => {
-  // const bucket = event.Records[0].s3.bucket.name;
-  // const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
-
-  // TODO implement
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify(results),
-  };
-
-  return new Promise((resolve, reject) => {
+exports.handler = async event =>
+  new Promise((resolve, reject) => {
     makeRedshift()
       .then(redshift => redshift.query('select * from information_schema.tables'))
       .then(res => {
@@ -42,4 +33,3 @@ exports.handler = async event => {
         resolve(res);
       });
   });
-};
