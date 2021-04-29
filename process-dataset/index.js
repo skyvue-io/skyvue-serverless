dc58fc64003a16c86640826d2809c555611388e6
Tristan Tarpley <tristan@learnwithjuni.com>
2021-04-28 22:31:12 -0500

    testing redshift integration

--- process-dataset/index.js
+++ process-dataset/index.js
@@ -1,8 +1,38 @@
-exports.handler = async event => {
-  const bucket = event.Records[0].s3.bucket.name;
-  const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
+require('dotenv').connect();
+const { Client } = require('pg');
 
-  console.log('successful invokation!!', bucket, key);
+const {
+  REDSHIFT_DB,
+  REDSHIFT_USER,
+  REDSHIFT_PW,
+  REDSHIFT_PORT,
+  REDSHIFT_HOST,
+} = process.env;
+
+const dbParams = {
+  user: REDSHIFT_USER,
+  database: REDSHIFT_DB,
+  password: REDSHIFT_PW,
+  port: REDSHIFT_PORT,
+  host: REDSHIFT_HOST,
+};
+
+const makeRedshift = async () => {
+  const client = new Client(dbParams);
+  await client.connect();
+
+  return client;
+};
+
+exports.handler = async event => {
+  // const bucket = event.Records[0].s3.bucket.name;
+  // const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
+
+  const redshift = await makeRedshift(client);
+
+  // console.log('successful invokation!!', bucket, key);
+  console.log(await redshift.query('select * from information_schema.tables'));
+
   // TODO implement
   const response = {
     statusCode: 200,
