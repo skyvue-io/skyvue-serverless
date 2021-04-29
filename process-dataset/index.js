b51065615596520cf5b473afb8c2da8f6cd9da52
Tristan Tarpley <tristan@learnwithjuni.com>
2021-04-28 21:07:44 -0500

    test

--- process-dataset/index.js
+++ process-dataset/index.js
@@ -2,7 +2,7 @@ exports.handler = async event => {
   const bucket = event.Records[0].s3.bucket.name;
   const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
 
-  console.log('successful invokation', bucket, key);
+  console.log('successful invokation!', bucket, key);
   // TODO implement
   const response = {
     statusCode: 200,
