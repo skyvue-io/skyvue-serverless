4c6b93c5b2c902fb19bb720a0302492c90273cee
Tristan Tarpley <tristan@learnwithjuni.com>
2021-05-01 11:33:50 -0500

    connect using default env vars

--- process-dataset/index.js
+++ process-dataset/index.js
@@ -2,25 +2,8 @@ require('dotenv').config();
 const { Client } = require('pg');
 const axios = require('axios');
 
-const {
-  REDSHIFT_DB,
-  REDSHIFT_USER,
-  REDSHIFT_PW,
-  REDSHIFT_PORT,
-  REDSHIFT_HOST,
-} = process.env;
-
 exports.handler = async (event, context) => {
-  const dbParams = {
-    user: REDSHIFT_USER,
-    database: REDSHIFT_DB,
-    password: REDSHIFT_PW,
-    port: REDSHIFT_PORT,
-    host: REDSHIFT_HOST,
-  };
-  const client = new Client(dbParams);
-  const connected = await client.authenticate();
-  console.log(connected);
+  const client = new Client();
   await client.connect();
 
   const results = await client.query('select * from information_schema.tables');
