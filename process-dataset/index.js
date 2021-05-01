fa25cc117a0276d3e902fb933f6dbae8ce2212c0
Tristan Tarpley <tristan@learnwithjuni.com>
2021-05-01 14:40:44 -0500

    new deps and set up a few imports

--- process-dataset/index.js
+++ process-dataset/index.js
@@ -1,19 +1,41 @@
 require('dotenv').config();
+
 const { Client } = require('pg');
+const aws = require('aws-sdk');
+const knex = require('knex')({
+  client: 'redshift',
+});
+const R = require('ramda');
+
+const parseDataType = require('./parseDataType');
+
+const awsConfig = new aws.Config({
+  region: 'us-east-2',
+  accessKeyId: process.env.AWS_ACCESSKEY,
+  secretAccessKey: process.env.AWS_SECRET_ACCESSKEY,
+});
+const s3 = new aws.S3(awsConfig);
+
+const redshift = new Client();
 
 exports.handler = async (event, context) => {
-  const client = new Client();
-  console.log('I am called', process.env.PGHOST);
-  await client.connect();
+  await redshift.connect();
+  const Bucket = event.Records[0].s3.bucket.name;
+  const Key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
+  const params = {
+    Bucket,
+    Key,
+  };
 
-  // const test = await axios.get('https://swapi.dev/api/people/1/');
-  // console.log(test.data);
+  // extract column information
+  // parse data types
+  // upload columns to S3 or Mongo? TODO figure this out
+  // generate create table query according to colIds
+  // generate insert query
+  // execute itttt
 
-  // return new Promise((resolve, reject) => {
-  //   axios.get('https://swapi.dev/api/people/1/').then(data => resolve(data.data));
-  // });
   return new Promise((resolve, reject) => {
-    client
+    redshift
       .query('select * from information_schema.tables')
       .then(data => resolve(data.rows));
   });
