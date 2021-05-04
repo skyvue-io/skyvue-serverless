0447f1201f85beeeecbec4cd3951c5c2a38c12cb
Tristan Tarpley <tristan@learnwithjuni.com>
2021-05-04 18:39:54 -0500

    tweak loz

--- process-dataset/index.js
+++ process-dataset/index.js
@@ -57,7 +57,7 @@ exports.handler = async (event, context) => {
   console.log(createUnprocessedTableQuery(key.slice(0, -1), columns));
   await redshift.query(createUnprocessedTableQuery(key.slice(0, -1), columns));
 
-  console.log(createUnloadSelectQuery(key.slice(0, -2), columns));
+  console.log(createUnloadQuery('select * from test', boardId));
   await redshift.query(
     createUnloadQuery(createUnloadSelectQuery(key.slice(0, -2), columns), boardId),
   );
